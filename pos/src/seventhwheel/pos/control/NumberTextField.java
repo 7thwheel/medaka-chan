a401d5051af9ba5d35b83730865ce016dc8b0623
akifumi <aki.7thwheel@gmail.com>
2013-05-03 14:43:50 +0900

    イベントフィルターを実装しました。

--- pos/src/seventhwheel/pos/control/NumberTextField.java
+++ pos/src/seventhwheel/pos/control/NumberTextField.java
@@ -6,15 +6,25 @@ import javafx.scene.input.KeyEvent;
 
 public class NumberTextField extends TextField {
 
-    public NumberTextField() {
-        addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+  public NumberTextField() {
+    addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
 
-            @Override
-            public void handle(KeyEvent event) {
-                if (!event.getCharacter().matches("[0-9]")) {
-                    event.consume();
-                }
-            }
-        });
-    }
+      @Override
+      public void handle(KeyEvent event) {
+        switch (event.getCode()) {
+        case ENTER:
+        case TAB:
+        case BACK_SPACE:
+          return;
+
+        default:
+          break;
+        }
+
+        if (event.getCharacter().matches("[^0-9]")) {
+          event.consume();
+        }
+      }
+    });
+  }
 }
